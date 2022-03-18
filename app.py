3d9cf1f08d222d09ced8e3260e0b9a0df411f4ef
Miranda Prasad <miranda_prasad@intuit.com>
2022-03-18 17:36:30 +0000

    final proj updates

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app.title=tabtitle
 
 ####### Layout of the app ########
 app.layout = html.Div([
-    html.H3('Choose a continuous variable for summary statistics:'),
+    html.H3('Chose a continuous variable to update the y axis:'),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in variables_list],
